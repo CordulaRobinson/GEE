4d5906aa789113cdaf7443467d0fe13be48edb9a
Raymond Eah <raymond.eah@gmail.com>
2022-03-22 22:05:55 -0400

    simple ee job

--- raymondeah/congo/auto/simplest/create_ten_plus_seven_job.py
+++ raymondeah/congo/auto/simplest/create_ten_plus_seven_job.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-bash_filename = 'basic_gee_job.sh'
+bash_filename = 'basic_gee_job'+str(small_grid)+'_'+str(lon_min)+'_'+str(lat_min)+'_'+str(lon_max)+'_'+str(lat_max)+'.sh'
 with open(bash_filename,'w') as f:
     # one node, one hour
     # name of job is DCR_i_j
@@ -13,7 +13,7 @@ with open(bash_filename,'w') as f:
     f.write('#!/bin/bash'+'\n')
     f.write('#SBATCH --nodes=1'+'\n')
     f.write('#SBATCH --time=01:00:00'+'\n')
-    f.write('#SBATCH --job-name=basic_gee_job'+'\n')
+    f.write('#SBATCH --job-name=ten_plus_seven'+str(i)+'_'+str(j)+'\n')
     f.write('#SBATCH --partition=short'+'\n')
     f.write('#SBATCH --mem=1GB'+'\n')
     f.write('module load anaconda3/3.7'+'\n')
@@ -21,7 +21,7 @@ with open(bash_filename,'w') as f:
     f.write('source activate ee'+'\n')
     f.write('conda activate ee'+'\n')
     f.write('conda init bash'+'\n')
-    f.write('python3 create_basic_gee_job.py'+'\n')
+    f.write('python3 add_two_numbers.py '+ ' 7 ' + ' 10' + '\n')
 
 # now we will submit the job (the bash_filename) written abobe
 os.system("sbatch "+str(bash_filename))
