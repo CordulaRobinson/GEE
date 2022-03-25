ec7e1cbcdca77642c6ae0474db510f9e2d1e3e9b
raymondeah <raymond.eah@gmail.com>
2022-03-25 12:29:19 -0400

    50x50 test run

---
+++ raymondeah/congo/auto/routine/batch/13_routine_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=01:00:00
+#SBATCH --job-name=routine_job
+#SBATCH --partition=short
+#SBATCH --mem=16GB
+module load anaconda3/3.7
+source activate 
+source activate ee
+conda activate ee
+conda init bash
+python3 mine_detection.py 28.695003740032092 29.14417103003744 -0.4491844498753762 -0.8983688997507524 13
---
+++ raymondeah/congo/auto/routine/batch/17_routine_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=01:00:00
+#SBATCH --job-name=routine_job
+#SBATCH --partition=short
+#SBATCH --mem=16GB
+module load anaconda3/3.7
+source activate 
+source activate ee
+conda activate ee
+conda init bash
+python3 mine_detection.py 27.347501870016046 27.796669160021395 -0.8983688997507524 -1.3475517757849604 17
