c5509d87f6e48eb89e9502c7f02ca267da7c4afa
raymondeah <raymond.eah@gmail.com>
2022-03-25 11:21:13 -0400

    multiple jobs test run 2

--- raymondeah/congo/auto/routine/batch/2_routine_job.sh
+++ raymondeah/congo/auto/routine/batch/2_routine_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=01:00:00
-#SBATCH --job-name=start_routine
+#SBATCH --job-name=routine_job
 #SBATCH --partition=short
-#SBATCH --mem=1GB
+#SBATCH --mem=16GB
 module load anaconda3/3.7
 source activate 
 source activate ee
 conda activate ee
 conda init bash
-python3 multiple_jobs.py 29.5 29.75 3.25 3
+python3 mine_detection.py 29.67966691600214 29.76950037400321 3.25 3.1601628704406277 2
