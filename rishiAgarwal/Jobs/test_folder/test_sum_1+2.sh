be763c3e3df5996896ebaf986768af310b8f9ea6
rxagar10 <agarwal.rishi@northeastern.edu>
2022-03-25 13:30:11 -0400

    troubleshooting

--- rishiAgarwal/Jobs/test_folder/test_sum_1+2.sh
+++ rishiAgarwal/Jobs/test_folder/test_sum_1+2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=1:00:00
-#SBATCH --job-name=Routine_Squares_CSV_Export
+#SBATCH --time=01:00:00
+#SBATCH --job-name=DCR_1_2
 #SBATCH --partition=short
-
+#SBATCH --mem=12GB
 module load anaconda3/3.7
-source activate
+source activate 
 source activate ee
 conda activate ee
 conda init bash
-python3 submit_jobs_test.py
+python3 add_two_numbers.py 2 3
