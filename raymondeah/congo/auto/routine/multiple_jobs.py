2c0500d46a642a17c16714e834e96987ecbe44fe
Raymond Eah <raymond.eah@gmail.com>
2022-03-25 10:09:37 -0400

    nested jobs first try

--- raymondeah/congo/auto/routine/multiple_jobs.py
+++ raymondeah/congo/auto/routine/multiple_jobs.py
@@ -39,6 +39,7 @@ def create_segments(geometry, size):
     width = int(ee.Geometry.Point(coords.get(0)).distance(ee.Geometry.Point(coords.get(1))).divide(1000 * size).getInfo())
     height = int(ee.Geometry.Point(coords.get(1)).distance(ee.Geometry.Point(coords.get(2))).divide(1000 * size).getInfo())
 
+    count = 0
     for y in range(height + 1):
         left = ee.Number(ee.List(coords.get(0)).get(0))
         for x in range(width + 1):
@@ -55,14 +56,32 @@ def create_segments(geometry, size):
             new_lat = left.add((dy.divide(r_earth)).multiply((ee.Number(180).divide(pi))))
             
             # create and submit jobs here
-            square = ee.Geometry.Polygon(
-                [[[left, new_lon],
-                  [new_lat, new_lon],
-                  [new_lat, top],
-                  [left, top]]])
-            
-            #segments.append(square)
-            
+            bash_filename = 'batch/' + str(count) + '_routine_job.sh'
+            with open(bash_filename,'w') as f:
+                # one node, one hour
+                # name of job is DCR_i_j
+                # # 12 GB memory
+                # load up conda
+                # execute 'gee_dcr.py' (your jupyter codes turned to .py)
+                # the arguments passed are the bounding box (1x1 degree for now)
+                # plus the high res smaple size == 0.0025 degree
+                f.write('#!/bin/bash'+'\n')
+                f.write('#SBATCH --nodes=1'+'\n')
+                f.write('#SBATCH --time=01:00:00'+'\n')
+                f.write('#SBATCH --job-name=routine_job'+'\n')
+                f.write('#SBATCH --partition=short'+'\n')
+                f.write('#SBATCH --mem=16GB'+'\n')
+                f.write('module load anaconda3/3.7'+'\n')
+                f.write('source activate '+'\n')
+                f.write('source activate ee'+'\n')
+                f.write('conda activate ee'+'\n')
+                f.write('conda init bash'+'\n')
+                f.write('python3 mine_detection.py ' + sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + sys.argv[4] + ' ' + count + '\n')
+
+            # now we will submit the job (the bash_filename) written abobe
+            os.system("sbatch "+str(bash_filename))
+
+            count += 1
             left = new_lat
         top = new_lon
         
