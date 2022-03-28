969840c7a27615693173f5437a6ad77c8c594f60
raymondeah <raymond.eah@gmail.com>
2022-03-28 09:54:23 -0400

    starting compile script

---
+++ raymondeah/congo/auto/routine/compile_and_convert.py
@@ -0,0 +1,14 @@
+import os
+import csv
+
+# os.system("cat output/*csv > output/compiled.csv")
+# os.system("rm -rf slurm*")
+# os.system("rm -rf output/square*")
+# os.system("rm batch/*")
+
+# with open('output/compiled.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#     for row in spamreader:
+#         print(', '.join(row))
+
+print('1')
