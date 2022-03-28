5efcf57d54842a520dd79d5770acad32fda81de5
raymondeah <raymond.eah@gmail.com>
2022-03-28 10:39:43 -0400

    new csv writing test run

--- raymondeah/congo/auto/routine/compile_and_convert.py
+++ raymondeah/congo/auto/routine/compile_and_convert.py
@@ -3,20 +3,20 @@ import csv
 
 import ast
 
-# os.system("cat output/*csv > output/compiled.csv")
-# os.system("rm -rf slurm*")
-# os.system("rm -rf output/square*")
-# os.system("rm batch/*")
+os.system("cat output/*csv > output/compiled.csv")
+os.system("rm -rf slurm*")
+os.system("rm -rf output/square*")
+os.system("rm batch/*")
 
-path = "c:/Users/r.eah/OneDrive - Northeastern University/gee/raymondeah/congo/auto/routine/output/compiled.csv"
+# path = "c:/Users/r.eah/OneDrive - Northeastern University/gee/raymondeah/congo/auto/routine/output/compiled.csv"
 
-restore = []
-with open(path, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ')
-    for row in spamreader:
-        restore.append(row)
+# restore = []
+# with open(path, newline='') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=' ')
+#     for row in spamreader:
+#         restore.append(row)
 
-for row in restore[:5]:
-    print(row)
+# for row in restore[:5]:
+#     print(row)
 
 
