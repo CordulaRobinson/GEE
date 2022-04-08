ba47da1d83dbce48fd45303f9d4734751e76247b
rxagar10 <agarwal.rishi@northeastern.edu>
2022-04-08 12:36:00 -0400

    added final code

---
+++ rishiAgarwal/Jobs/final/step4_rerun_failed_jobs.py
@@ -0,0 +1,27 @@
+import os
+import glob
+        
+# remove all slurm files with no exceptions
+os.system("find | grep -l -L -E exception output/slurm* | xargs rm -f")
+
+# create a txt file containing job numbers of all failed jobs
+# os.system("grep JOB output/slurm* > failed.txt")
+# with open('failed.txt') as file:
+#     for line in file:
+#         # get the index of the job number's location
+#         i = line.index('JOB')
+#         # get the job number
+#         job_number = int(line[i:].rstrip()[-1]) + 1000000
+#         # resubmit the failed job
+#         os.system('find batch -name routine_job_' + str(job_number) + '.sh | xargs sbatch')
+
+os.chdir("/scratch/nason.e/gee/output") # change to your own path to the 'output' folder
+extension = 'out'
+filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+os.chdir("/scratch/nason.e/gee") # change to your own path
+for file in filenames:
+    # get job number by indexing into the file name
+    job_number = int(file[6:-4])
+    # resubmit the failed job
+    os.system('find batch -name routine_job_' + str(job_number) + '.sh | xargs sbatch')
---
+++ rishiAgarwal/Jobs/final/step5_compile.py
@@ -0,0 +1,30 @@
+import os
+import csv
+#import ee
+import ast
+import pandas as pd
+import glob
+
+# Put all relevant CSV files into a list
+os.chdir("/scratch/nason.e/gee/results") # change to your own path to the 'results' folder
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+# Header
+header_list = ['Mininum Longitude', 'Minimum Latitude', 'Maximum Longitude', 'Maximum Latitude', \
+      'Percent Vegetation Loss', 'Percent Bare Initial','Percent Significant VH Values', 'Average NIR/G', 'Average SWIR1/B']
+
+# Combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+# Export to csv, file name = compiled.csv, with header
+combined_csv.to_csv( "compiled.csv", header=header_list, index=False, encoding='utf-8-sig')
+
+# Remove extraneous files
+os.chdir("/scratch/nason.e/gee") # change to your own path
+os.system("rm -rf results/job*")
+os.system("rm -rf output/slurm*")
+os.system("rm -rf batch/routine*")
+os.system("rm queue.txt")
+os.system("rm failed.txt")
+os.system("rm -rf slurm*")
+os.system("rm start_routine.sh")
