92ef53db03751f3cfcc37472b7032f776ac46f4d
Emily Nason <97188179+emilynason@users.noreply.github.com>
2022-04-26 14:09:54 -0400

    wrong file name

--- GEE_Mines/step5_compile.py
+++ GEE_Mines/step5_compile.py
@@ -9,4 +9,4 @@ os.system("rm -rf batch/routine*") # remove batch files
 os.system("rm queue.txt") # remove temp queue file
 os.system("rm failed.txt") # remove failed jobs txt file
 os.system("rm start_routine.sh") # remove step 1 bash file
-os.system("python3 add_scores.py compiled") # call next step with desired csv file name (do not include .csv)
+os.system("python3 step5a_add_scores.py compiled") # call next step with desired csv file name (do not include .csv)
