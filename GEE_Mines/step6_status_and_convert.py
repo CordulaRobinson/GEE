77e585366e64c2f6f501ab61de621c01a1298427
Emily Nason <97188179+emilynason@users.noreply.github.com>
2022-04-26 13:54:04 -0400

    add file_name to step6, reformat CSVs and FeatureCollection
    formalized file naming for step6, updated functions for additional CSV columns
    removed elevation loss from status calculation
    added elevation score and band variation score as properties to the final FeatureCollection - these can be filtered on within google earth engine for final results

--- GEE_Mines/step6_status_and_convert.py
+++ GEE_Mines/step6_status_and_convert.py
@@ -6,9 +6,11 @@ ee.Initialize()
 # All results will be saved in "compiled_status.csv"
 # Passing region results only will be saved in "compiled_status_passing.csv"
 
+file_name = sys.argv[1]
+
 # Open the input file in read mode and output file in write mode
-with open('results/compiled.csv', 'r') as read_obj, \
-        open('results/compiled_status.csv', 'w', newline='') as write_obj:
+with open('results/' + file_name + '_scores.csv', 'r') as read_obj, \
+        open('results/' + file_name + '_status.csv', 'w', newline='') as write_obj:
     # Create a csv.reader object from the input file object
     csv_reader = reader(read_obj)
     # Create a csv.writer object from the output file object
@@ -16,17 +18,16 @@ with open('results/compiled.csv', 'r') as read_obj, \
     # Add header to output file, with status column
     # Add header to output file, with status column
     header_list = ['Mininum Longitude', 'Minimum Latitude', 'Maximum Longitude', 'Maximum Latitude', \
-                    'Percent Vegetation Loss', 'Percent Bare Initial', 'Percent Significant VH Values', \
-                    'Average NIR/G', 'Average SWIR1/B', 'NASA Elev', 'GEDI Elev', 'Elev Loss','GEDI Qual. Flag',\
-                        'B5 Value', 'B6 Value', 'Status']
+                   'Percent Vegetation Loss', 'Percent Bare Initial', 'Percent Significant VH Values', \
+                   'Average NIR/G', 'Average SWIR1/B', 'NASA Elev', 'GEDI Elev', 'Elev Loss','GEDI Qual. Flag',\
+                   'B5 Value', 'B6 Value', 'Center Lon', 'Center Lat','Elevation Score', \
+                   'Band Variation Score','Status']
     csv_writer.writerow(header_list)
     # Read each row of the input csv file as list
     for row in csv_reader:
         # Calculate Status and append to the end of the row/list
         # Passing: (Veg loss > 20% or Initial Bare Earth > 10%) and SAR VH > 5% and NIR/G <= 0.45 and SWIR1/B < 0.65 
-        # and Elevation Loss < 0 (this also accounts for missing data = -999)
-        status = (((float(row[4]) > 20) or (float(row[5]) > 10))and (float(row[6]) > 5) and (float(row[7]) <= 0.45) and (float(row[8]) < 0.65) \
-                 and (float(row[11]) < 0))
+        status = (((float(row[4]) > 20) or (float(row[5]) > 10))and (float(row[6]) > 5) and (float(row[7]) <= 0.45) and (float(row[8]) < 0.65))
         if status:
             row.append("Pass")
         else: 
@@ -35,8 +36,8 @@ with open('results/compiled.csv', 'r') as read_obj, \
         csv_writer.writerow(row)
 
 # Create a file of only passing statuses
-with open('results/compiled_status.csv', 'r') as r, \
-        open('results/compiled_passing.csv', 'w', newline='') as w:
+with open('results/' + file_name + '_status.csv', 'r') as r, \
+        open('results/' + file_name + '_status_passing.csv', 'w', newline='') as w:
     # Create a csv.reader object from the input file object
     csv_reader = reader(r)
     # Create a csv.writer object from the output file object
@@ -48,12 +49,12 @@ with open('results/compiled_status.csv', 'r') as r, \
     if header != None:
         # Add passing rows to new file
         for row in csv_reader:
-            if row[15] == "Pass":
+            if row[19] == "Pass":
                 csv_writer.writerow(row)
                 
 # Convert to a Feature Collection
 # Create a list of Geometries for areas that passed as possible mines
-with open('results/compiled_passing.csv', 'r') as r:
+with open('results/' + file_name + '_status_passing.csv', 'r') as r:
     csv_reader = reader(r)
     # Skip Header in input file
     header = next(csv_reader)
@@ -65,6 +66,7 @@ with open('results/compiled_passing.csv', 'r') as r:
                       [float(row[0]), float(row[1])],
                       [float(row[2]), float(row[1])],
                       [float(row[2]), float(row[3])]]])
+            f = ee.Feature(region).set('elevation score', float(row[17])).set('b5/b6 score', float(row[18]))
             region_list.append(region)
 
 # Wrap geometry list in a Feature Collection
@@ -74,7 +76,7 @@ fc = ee.FeatureCollection(region_list)
 task = ee.batch.Export.table.toAsset(**{
   'collection': fc,
   'description':'compiled_results',
-  'assetId': 'users/EmilyNason/compiledResults', # change to your GEE Asset path and a unique name (will not overwrite already existing assets, so old names cannot be reused)
+  'assetId': 'users/EmilyNason/FinalResults', # change to your GEE Asset path and a unique name (will not overwrite already existing assets, so old names cannot be reused)
 });
 
 task.start()
