d2a803254d3975ab0280ab5ffdc87da0227a2aa8
conwayek <e.conway@northeastern.edu>
2022-04-28 11:29:57 -0400

    file name change:add date

--- Hyperion/get_Hyperion_ReGrid.py
+++ Hyperion/get_Hyperion_ReGrid.py
@@ -19,7 +19,7 @@ def regrid(inlst):
     N_bands = len(bands)
     data = np.zeros((m,N_bands))
     lon = np.zeros(m) ; lat = np.zeros(m)
-    for i in tqdm(range(m)):
+    for i in range(m):
         lon[i] = inlst[i]['geometry']['coordinates'][0]
         lat[i] = inlst[i]['geometry']['coordinates'][1]   
         for j in range(N_bands):
@@ -36,7 +36,7 @@ def regrid(inlst):
     idx=np.argsort(lat)
     lat_sort=lat[idx]
     xx,yy = np.meshgrid(lon_sort,lat_sort)
-    for i in tqdm(range(N_bands)):
+    for i in range(N_bands):
         interp = LinearNDInterpolator((lon,lat),data[:,i])
         data_cube[:,:,i] = interp((xx,yy))
     return xx,yy,data_cube,bnd_index
@@ -48,7 +48,7 @@ ee.Initialize()
 Define Region Bounds: Lower Left, Top Right of a bounding box
 """
 # Mine 1.
-lon_ll = 25.79 ; lon_tr = 25.83 ; lat_ll = -10.79 ; lat_tr= -10.77
+lon_ll = 26.59 ; lon_tr = 26.63 ; lat_ll = -10.89 ; lat_tr= -10.88
 
 # meter resolution per pixel, native = 30m
 # caution, there are almost 200 bands. Arrays can be extremely large.
@@ -67,17 +67,24 @@ Number of measurements obtained, could be zero, could be many
 """
 Nmeasurements = hyper.size().getInfo()
 
+print(Nmeasurements)
+#if(Nmeasurements != 0):
+#    print(hyper.getInfo())
 
-if(hyper.size().getInfo() != 0):
-    print('Found ', hyper.size().getInfo(),' Hyperion measurements, retrieving and re-gridding them now.....')
+
+if(Nmeasurements != 0):
+    print('Found ', Nmeasurements,' Hyperion measurements, retrieving and re-gridding them now.....')
     # convert the entire dataset to a list for GEE (server side)
     hyper_list = hyper.toList(hyper.size())
-    for i in range(hyper.size().getInfo()):
+    for i in tqdm(range(Nmeasurements)):
         # create an image of the i'th measurement on the server
         hyper_i = ee.Image(hyper_list.get(i))#.clip(geom)
         sza = hyper_i.get('SENSOR_LOOK_ANGLE').getInfo()
         second = hyper_i.get('system:time_start').getInfo()
-        date_aquisition = dt.datetime(year=1970,month=1,day=1) + dt.timedelta(seconds=second/1000)
+        date_acquisition = dt.datetime(year=1970,month=1,day=1) + dt.timedelta(seconds=second/1000)
+        year = date_acquisition.year
+        month = date_acquisition.month
+        day = date_acquisition.day
         sc_sw = hyper_i.get('SCALING_FACTOR_SWIR').getInfo()
         sc_ir = hyper_i.get('SCALING_FACTOR_VNIR').getInfo()
         
@@ -96,8 +103,8 @@ if(hyper.size().getInfo() != 0):
                 scale[j] = sc_sw
             elif(bnd_indices[j] >=8 and bnd_indices[j] <=57):
                 scale[j] = sc_ir
-        
-        fname = os.path.join(os.getcwd(),'Hyperion_'+str(lon_ll)+'_'+str(lat_ll)+'_'+str(lon_tr)+'_'+str(lat_tr)+'.nc')
+        tm = str(year)+'-'+str(month)+'-'+str(day)
+        fname = os.path.join(os.getcwd(),'Hyperion_'+str(lon_ll)+'_'+str(lat_ll)+'_'+str(lon_tr)+'_'+str(lat_tr)+'_'+tm+'.nc')
         with nc4.Dataset(fname,'w', format='NETCDF4') as f:
             f.createDimension('Pixel',lon.shape[0])
             f.createDimension('Bands',data_array.shape[2])
