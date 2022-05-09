569cc52206fc57967ae691dfe61db62415912465
Raymond Eah <raymond.eah@gmail.com>
2022-05-09 14:48:18 -0400

    drc folder 3/4

---
+++ drc/automation/basic_gee/basic_gee_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=01:00:00
+#SBATCH --job-name=basic_gee_job
+#SBATCH --partition=short
+#SBATCH --mem=1GB
+module load anaconda3/3.7
+source activate 
+source activate ee
+conda activate ee
+conda init bash
+python3 basic_gee.py
